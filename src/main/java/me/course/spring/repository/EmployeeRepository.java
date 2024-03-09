0efbc89d9e6ad438aeeb196560315a10edb3502a
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-03-09 14:48:46 +0300

    create and impl EmployeeRepository

---
+++ src/main/java/me/course/spring/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package me.course.spring.repository;
+
+import me.course.spring.entity.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
