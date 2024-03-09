f4c4f4f416e083a5ce139153c16eea6365cf293c
ElenaM <115351366+LenaMishanina@users.noreply.github.com>
2024-03-09 12:58:54 +0300

    add unversioned files

---
+++ src/main/java/me/course/spring/jpahibernateproject2/JpaHibernateProject2Application.java
@@ -0,0 +1,13 @@
+package me.course.spring.jpahibernateproject2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateProject2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHibernateProject2Application.class, args);
+	}
+
+}
